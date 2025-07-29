ed60934bbb07c57c18beda0341d88eeed8dc4382
vaddy_b <vadim.krsk.94@gmail.com>
2025-07-30 01:20:37 +0700

    1.14 strings

---
+++ src/main/java/ru/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+
+        String idea = "I like Java!";
+        int year = 2025;
+        System.out.println(idea);
+        idea += "But I am a newbie.";
+        idea += year;
+        System.out.println(idea);
+    }
+}
--- src/main/java/ru/condition/SqArea.java
+++ src/main/java/ru/condition/SqArea.java
@@ -2,10 +2,8 @@ package ru.condition;
 
 public class SqArea {
     public static double square(double p, double k) {
-
         double h = p / (2 * (k + 1));
         double l = h * k;
-
         double rsl = l * h;
         return rsl;
     }
