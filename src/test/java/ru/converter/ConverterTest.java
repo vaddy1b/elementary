f6007e97217c265c14ec854999033aead9158010
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-02 16:04:58 +0700

    2.1 module tests refactoring

--- src/test/java/ru/converter/ConverterTest.java
+++ src/test/java/ru/converter/ConverterTest.java
@@ -20,7 +20,7 @@ class ConverterTest {
     void whenConvert140RblThen1Dot55Dollar() {
         float in = 140;
         float expected = 1.55f;
-        float out = Math.round(Converter.rubleToDollar(in) * 100) / 100.0f;
+        float out = Converter.rubleToDollar(in);
         float eps = 0.01f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
@@ -29,8 +29,8 @@ class ConverterTest {
     void whenConvert200RblThen2Dot22Dollar() {
         float in = 200;
         float expected = 2.22f;
-        float out = Math.round(Converter.rubleToDollar(in) * 100) / 100.0f;
-        float eps = 0.0001f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.01f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
