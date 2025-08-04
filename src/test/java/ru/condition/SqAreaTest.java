9a3f59250c63aec9ef69591fcb8fd6a15318be58
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-04 21:26:40 +0700

    2.6 tests rectangle sides

---
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -0,0 +1,36 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP2K3Square2() {
+        double expected = 0.18;
+        int p = 2;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP3K4Square2() {
+        double expected = 0.36;
+        int p = 3;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
