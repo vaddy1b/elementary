88134da05c1b4594a21c2ac050ae6b2d842d0b39
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-12 00:02:13 +0700

    3.14 chess elephant

--- src/main/java/ru/condition/LogicNot.java
+++ src/main/java/ru/condition/LogicNot.java
@@ -14,7 +14,7 @@ public class LogicNot {
     }
 
     public static boolean notPositive(int num) {
-        return num < 0;
+        return !isPositive(num) && num != 0;
     }
 
     public static boolean notEvenAndPositive(int num) {
