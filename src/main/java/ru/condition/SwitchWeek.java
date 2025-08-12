362d13d64c847d9a21731b8242201e0fa995325d
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-12 22:55:56 +0700

    3.17 switch case

---
+++ src/main/java/ru/condition/SwitchWeek.java
@@ -0,0 +1,41 @@
+package ru.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name = null;
+        if (day > 0 && day <= 7) {
+            switch (day) {
+                case 1:
+                    name = "Понедельник";
+                    break;
+
+                case 2:
+                    name = "Вторник";
+                    break;
+
+                case 3:
+                    name = "Среда";
+                    break;
+
+                case 4:
+                    name = "Четверг";
+                    break;
+
+                case 5:
+                    name = "Пятница";
+                    break;
+
+                case 6:
+                    name = "Суббота";
+                    break;
+
+                case 7:
+                    name = "Воскресенье";
+                    break;
+            }
+        } else {
+            return "Ошибка";
+        }
+        return name;
+    }
+}
