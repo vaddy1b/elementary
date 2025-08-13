17d3f87392e74ac7391679773adc81688c3bb0a9
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-13 23:27:19 +0700

    4.4 even sum of numbers

---
+++ src/test/java/ru/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumOfAllNumbersFromZeroToTenThenFiftyFive() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
+    }
+}
