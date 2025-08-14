404f3e64cf55c6dcbc754dd3c18b60b71b1e8f50
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-14 22:46:42 +0700

    4.5 factorial calculations fixed

--- src/main/java/ru/loop/Factorial.java
+++ src/main/java/ru/loop/Factorial.java
@@ -3,15 +3,9 @@ package ru.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        if (n > 0) {
             for (int i = result; i <= n; i++) {
                 result *= i;
             }
-        }
         return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(calc(1));
     }
 }
