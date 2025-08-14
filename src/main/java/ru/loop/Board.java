cf257dd1c0c4ab70174b560e5664099486b0819b
vaddy_b <vadim.krsk.94@gmail.com>
2025-08-14 23:23:07 +0700

    4.6 paint picture

--- src/main/java/ru/loop/Board.java
+++ src/main/java/ru/loop/Board.java
@@ -1,12 +1,9 @@
 package ru.loop;
 
 public class Board {
-
     public static void paint(int width, int height) {
         for (int row = 0; row < height; row++) {
             for (int cell = 0; cell < width; cell++) {
-                /* условие проверки, что писать пробел или X */
-                /* Выше в задании мы определили закономерность, когда нужно проставлять X */
                 if (row == cell || cell == width - 1 - row) {
                     System.out.print("X");
                 } else {
