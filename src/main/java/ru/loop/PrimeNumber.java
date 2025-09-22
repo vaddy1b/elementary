58bd277b72108a9623680b0a1b01acff0803bbac
vaddy_b <vadim.krsk.94@gmail.com>
2025-09-23 00:49:29 +0700

    4.9 prime numbers

---
+++ src/main/java/ru/loop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+
+        for (int i = 2; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
