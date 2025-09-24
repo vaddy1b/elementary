4f8259579c3c4bea9eab106feeeca71d7cb9acc5
vaddy_b <vadim.krsk.94@gmail.com>
2025-09-24 18:13:30 +0700

    5.2 arrays size

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short [] ages = new short[10];
         String [] surnames = new String[100500];
         float [] prices = new float[40];
+
+        System.out.println("Array size : " + ages.length);
+        System.out.println("Array size : " + surnames.length);
+        System.out.println("Array size : " + prices.length);
     }
 }
