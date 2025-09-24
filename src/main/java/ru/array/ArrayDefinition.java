e65e148205a851781c89b3e716332e5b4f3fcfd4
vaddy_b <vadim.krsk.94@gmail.com>
2025-09-25 02:38:27 +0700

    5.3 arrays filling + fixed spaces from 5.1

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -2,12 +2,23 @@ package ru.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short [] ages = new short[10];
-        String [] surnames = new String[100500];
-        float [] prices = new float[40];
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Ivan Ivanov";
+        names[1] = "Oleg Harchenko";
+        names[2] = "Semen Uglov";
+        names[3] = "Georgiy Leonidov";
 
         System.out.println("Array size : " + ages.length);
         System.out.println("Array size : " + surnames.length);
         System.out.println("Array size : " + prices.length);
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
