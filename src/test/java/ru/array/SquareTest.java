351bf475299d947f9a34c8f35ec96e92df1a37b5
vaddy_b <vadim.krsk.94@gmail.com>
2025-09-27 23:25:56 +0700

    5.7 test arrays

---
+++ src/test/java/ru/array/SquareTest.java
@@ -0,0 +1,15 @@
+package ru.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SquareTest {
+    @Test
+    void whenBound5Then014916() {
+        int bound = 5;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[]{0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
+    }
+}
