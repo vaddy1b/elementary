bd940f5ab49c8b3a3d26942ac0a36edeead3e3a0
vaddy_b <vadim.krsk.94@gmail.com>
2025-10-01 00:05:58 +0700

    5.8 fixed search 4

--- src/main/java/ru/array/FindLoop.java
+++ src/main/java/ru/array/FindLoop.java
@@ -3,7 +3,7 @@ package ru.array;
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-        for (int i : data) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 return i;
             }
