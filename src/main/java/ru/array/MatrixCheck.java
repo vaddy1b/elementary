1e138b454547ee0955fb35e3b79402e2762f264a
vaddy1b <eagle-vadim@mail.ru>
2025-10-12 16:46:20 +0700

    5.27 mono string in matrix

---
+++ src/main/java/ru/array/MatrixCheck.java
@@ -0,0 +1,15 @@
+package ru.array;
+
+public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, int row) {
+        for (int j = 0; j < board[row].length; j++) {
+            if (board[row][j] != 'X' && board[row][j] != ' ') {
+                return false;
+            }
+            if (board[row][j] != 'X') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/ru/array/MatrixCheckTest.java
@@ -0,0 +1,44 @@
+package ru.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MatrixCheckTest {
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenOneElementNotSpaceOrXThenFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'k', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenRowNotFullOfXThenFalse() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', ' ', 'X'},
+                {' ', ' ', ' '},
+        };
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
+        assertThat(result).isFalse();
+    }
+
+}
