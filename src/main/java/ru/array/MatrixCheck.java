eaf87fb838575b33208078f42e37404badd3abbe
vaddy1b <eagle-vadim@mail.ru>
2025-10-13 11:09:00 +0700

    5.27 mono string in matrix fixed

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -3,9 +3,6 @@ package ru.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         for (int j = 0; j < board[row].length; j++) {
-            if (board[row][j] != 'X' && board[row][j] != ' ') {
-                return false;
-            }
             if (board[row][j] != 'X') {
                 return false;
             }
